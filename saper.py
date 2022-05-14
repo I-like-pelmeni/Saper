7e2f112443ae1345b645c53044de4a5c7b243a18
I-like-pelmeni <maxtoo2017@gmail.com>
2022-05-14 17:37:35 +0300

    Add task

--- saper.py
+++ saper.py
@@ -9,11 +9,46 @@ from PyQt5.QtCore import *
 import random
 import time
 
+
+
 LEVELS = (
     (8, 10),
     (16, 40),
     (24, 99)
 )
+
+IMG_BOMB = QImage('./images/bomb.png')
+IMG_CLOCK = QImage('./images/clock.png')
+
+
+class Cell(QWidget):
+    """
+    Клетка игрового поля
+    """
+
+    def __init__(self, x, y, *args, **kwargs):
+        """
+        Конструктор клекти игровго поля
+        """
+        super().__init__(*args, **kwargs)
+        self.setFixedSize(20, 20)
+
+        self.x = x
+        self.y = y
+
+    def paintEvent(self, event):
+        """
+        Событие перерисовки клетки
+        """
+        p = QPainter(self)
+        p.setRenderHint(QPainter.Antialiasing)
+        r = event.rect()
+        outer, inner = Qt.gray, Qt.lightGray
+        p.fillRect(r, QBrush(inner))
+        pen = QPen(outer)
+        pen.setWidth(1)
+        p.setPen(pen)
+        p.drawRect(r)
 
 
 class MainWindow(QMainWindow):
@@ -31,8 +66,9 @@ class MainWindow(QMainWindow):
         self.board_size, self.n_mines = LEVELS[self.level]
 
         self.setWindowTitle('Сапер')
-        self.setFixedSize(300, 300)
         self.initUI()
+        self.init_map()
+        self.setFixedSize(self.sizeHint())
         self.show()
 
     def initUI(self):
@@ -60,15 +96,38 @@ class MainWindow(QMainWindow):
         self.button.setIcon(QIcon('./images/smiley.png'))
         self.button.setFlat(True)
 
+        l = QLabel()
+        l.setPixmap(QPixmap.fromImage(IMG_BOMB))
+        l.setAlignment(Qt.AlignCenter)
+        hb.addWidget(l)
+
         hb.addWidget(self.mines)
         hb.addWidget(self.button)
         hb.addWidget(self.clock)
 
+        l = QLabel()
+        l.setPixmap(QPixmap.fromImage(IMG_CLOCK))
+        l.setAlignment(Qt.AlignCenter)
+        hb.addWidget(l)
+
         vb = QVBoxLayout()
         vb.addLayout(hb)
 
+        self.grid = QGridLayout()
+        self.grid.setSpacing(5)
+        vb.addLayout(self.grid)
+
         w.setLayout(vb)
         self.setCentralWidget(w)
+
+    def init_map(self):
+        """
+        Инициализация игрового поля
+        """
+        for x in range(self.board_size):
+            for y in range(self.board_size):
+                w = Cell(x, y)
+                self.grid.addWidget(w, x, y)
 
 
 if __name__ == '__main__':
